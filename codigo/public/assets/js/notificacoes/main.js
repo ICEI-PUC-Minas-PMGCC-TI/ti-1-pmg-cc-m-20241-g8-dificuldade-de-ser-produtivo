ffa73bfed30241c28a434731bd59d8fce9c02ba3
mateushmd <mateushmdiniz@gmail.com>
2024-06-29 23:07:30 -0300

    Ajuste da barra lateral

--- codigo/public/assets/js/notificacoes/main.js
+++ codigo/public/assets/js/notificacoes/main.js
@@ -4,58 +4,47 @@ const mes = String(data.getMonth() + 1).padStart(2, '0')
 const ano = data.getFullYear()
 CurrentDate = `${ano}-${mes}-${dia}`
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function ()
+{
 
-/*
-    -------------------Ícone da barra-------------------*/
+    /*
+        -------------------Ícone da barra-------------------*/
     const notificationsIcon = document.getElementById('nav-notifications');
     const mainContainer = document.querySelector('.main-container');
 
-    notificationsIcon.addEventListener('click', function (event) {
+    notificationsIcon.addEventListener('click', function (event)
+    {
         event.preventDefault(); // Evita o comportamento padrão do link
         mainContainer.classList.toggle('hidden');
     });
 
     const botaoNotificacoes = document.querySelectorAll('.botoes a');
 
-    botaoNotificacoes.forEach(botao => {
-        botao.addEventListener('click', function () {
+    botaoNotificacoes.forEach(botao =>
+    {
+        botao.addEventListener('click', function ()
+        {
             // Seleciona a notificação pai e a oculta
             const notification = this.closest('.notification');
             notification.classList.add('hidden');
         });
     });/*
     ---------------------------------------------------------*/
-    
-    fetch('/tasks')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro ao buscar o arquivo JSON');
-        }
-        return response.json();
-    })
-    .then(data => {
-        const datatarefa = data.tasks[0].term;
-        if (CurrentDate === datatarefa) { // tarefa atual
-            tarefaproxima();
-        }
-        if (CurrentDate > datatarefa) { // tarefa expirada
-            tarefaatrasada();
-        }
-    })
-    .catch(error => {
-        console.error('Erro ao processar o arquivo JSON:', error);
-    });
     /*
     -------------------NOTIFICAÇÃO PRÓXIMA-------------------*/
-    function tarefaproxima() {
+    function tarefaproxima()
+    {
         const notificacao = document.querySelector('#notificacoes-separadas');
-        if (!notificacao) {
+        if (!notificacao)
+        {
             console.error('Elemento #notificacoes-separadas não encontrado no DOM.');
-        } else {
+        } else
+        {
             fetch('/tasks')
-                .then(async res => {
-                    if (!res.ok) {
+                .then(async res =>
+                {
+                    if (!res.ok)
+                    {
                         throw new Error(res.status);
                     }
 
@@ -71,7 +60,8 @@ document.addEventListener("DOMContentLoaded", function () {
                         `;
                     perfil.appendChild(project);
                 })
-                .catch(error => {
+                .catch(error =>
+                {
                     console.error('Erro ao buscar dados do json:', error);
                 });
         }
@@ -79,14 +69,19 @@ document.addEventListener("DOMContentLoaded", function () {
     ---------------------------------------------------------*/
     /*
     ------------------NOTIFICAÇÃO ATRASADA-------------------*/
-    function tarefaatrasada() {
+    function tarefaatrasada()
+    {
         const notificacao = document.querySelector('#notificacoes-separadas');
-        if (!notificacao) {
+        if (!notificacao)
+        {
             console.error('Elemento #notificacoes-separadas não encontrado no DOM.');
-        } else {
+        } else
+        {
             fetch('/tasks')
-                .then(async res => {
-                    if (!res.ok) {
+                .then(async res =>
+                {
+                    if (!res.ok)
+                    {
                         throw new Error(res.status);
                     }
 
@@ -103,7 +98,8 @@ document.addEventListener("DOMContentLoaded", function () {
                         `;
                     perfil.appendChild(project);
                 })
-                .catch(error => {
+                .catch(error =>
+                {
                     console.error('Erro ao buscar dados do json:', error);
                 });
         }
